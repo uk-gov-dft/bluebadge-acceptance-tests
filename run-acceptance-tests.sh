290b7b0855c2c9d2f15c0e79a2d5b0855075feb3
Andrew Rea <email@andrewrea.co.uk>
2018-09-06 11:36:48 +0000

    Adding the build arguments

--- run-acceptance-tests.sh
+++ run-acceptance-tests.sh
@@ -7,6 +7,6 @@ do
   NAME=$(echo -n "$application" | cut -d, -f2)
   
   pushd "$NAME"
-  ./gradlew acceptanceTests
+  ./gradlew acceptanceTests -PbuildProfile=local -Dos=linux -Dheadless=true
   popd
 done
