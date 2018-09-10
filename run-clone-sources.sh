a837e2c49639a75216e160f4408ac54b754472ab
Andrew Rea <email@andrewrea.co.uk>
2018-09-10 14:39:19 +0000

    Changing the auth

--- run-clone-sources.sh
+++ run-clone-sources.sh
@@ -12,7 +12,7 @@ do
   echo "SHORTCODE $SHORTCODE NAME $NAME"
   BRANCH_NAME="${SHORTCODE}_BRANCH" 
   echo "BRANCH_NAME = ${!BRANCH_NAME}"
-  git clone "https://$GITHUB_CREDENTIAL@github.com/uk-gov-dft/$NAME.git"
+  git clone "https://$GITHUB_TOKEN:x-oauth-basic@github.com/uk-gov-dft/$NAME.git"
   pushd "$NAME"
   git checkout "${!BRANCH_NAME}"
   popd
