cb4fe7013072e816cf6715047346779145e835ca
Andrew Rea <email@andrewrea.co.uk>
2018-09-10 14:51:19 +0000

    Changing the file location

--- run-build-env-feature-file.sh
+++ run-build-env-feature-file.sh
@@ -3,15 +3,12 @@
 . common.sh
 . applications.sh
 
-TMP_FILE=env-feature.sh
-rm "$TMP_FILE" || true
-
 for application in "${APPLICATIONS[@]}"
 do
   SHORTCODE=$(echo -n "$application" | cut -d, -f1)
   NAME=$(echo -n "$application" | cut -d, -f2)
 
   pushd "$NAME"
-  echo "export ${SHORTCODE}_VERSION=$(cat VERSION-computed)" >> "$TMP_FILE"
+  echo "export ${SHORTCODE}_VERSION=$(cat VERSION-computed)" >> "env-feature.sh"
   popd
 done
